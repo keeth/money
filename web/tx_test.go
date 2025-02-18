0b6e67b91ad1bdeb0e909cd06685ac3262870c94
Keith Grennan <keith@nearlyfree.org>
2025-02-17 20:49:26 -0800

    tx tests passing

--- web/tx_test.go
+++ web/tx_test.go
@@ -25,10 +25,10 @@ type WebTxTestSuite struct {
 func (suite *WebTxTestSuite) SetupTest() {
 	t := suite.T()
 	ctx := context.Background()
-	gm := golangmigrator.New("db/migrations")
+	gm := golangmigrator.New("../db/migrations")
 	db := sqlitestdb.New(t, sqlitestdb.Config{Driver: "sqlite3"}, gm)
 	app := money.InitGlobalApp(db)
-	ofxFile, err := os.Open("testdata/ofx/bank1.qfx")
+	ofxFile, err := os.Open("../testdata/ofx/bank1.qfx")
 	defer ofxFile.Close()
 	assert.NoError(t, err)
 	app.ImportOFX(ctx, ofxFile)
@@ -43,13 +43,32 @@ func (t *WebTxTestSuite) TestGetTxs() {
 	t.NoError(err)
 	var b strings.Builder
 	t.NoError(txs.Render(&b))
-	println(b.String())
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(b.String()))
 	t.NoError(err)
 	t.Equal(1, doc.Find("html").Length())
 	t.Equal(1, doc.Find("table").Length())
 	rows := doc.Find("tr")
 	t.Equal(3, rows.Length())
+	lastRow := rows.Last()
+	t.Equal("/tx?before=2025-02-02+4447", lastRow.AttrOr("hx-get", ""))
+	t.Equal("afterend", lastRow.AttrOr("hx-swap", ""))
+	t.Equal("revealed", lastRow.AttrOr("hx-trigger", ""))
+
+	err, txs = GetTxs(ctx, model.GetTxsParams{
+		Before: "2025-02-02 4447",
+		Limit:  2,
+	})
+	t.NoError(err)
+	var b2 strings.Builder
+	t.NoError(txs.Render(&b2))
+	// net/html requires a full html document
+	doc2, err := goquery.NewDocumentFromReader(strings.NewReader("<html><head></head><body><table>" + b2.String() + "</table></body></html>"))
+	t.NoError(err)
+	t.Equal(1, doc2.Find("table").Length())
+	rows2 := doc2.Find("tr")
+	t.Equal(1, rows2.Length())
+	lastRow2 := rows2.Last()
+	t.Equal("/tx?before=2025-02-02+4446", lastRow2.AttrOr("hx-get", ""))
 }
 
 func TestWebTxTestSuite(t *testing.T) {
